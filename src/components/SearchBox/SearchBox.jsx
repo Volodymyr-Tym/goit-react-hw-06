5bbdbc8c4e43ef4ac0af47f42f59648189c323fc
Volodymyr-Tym <163361581+Volodymyr-Tym@users.noreply.github.com>
2024-10-29 00:11:27 +0000

    Make part of task

---
+++ src/components/SearchBox/SearchBox.jsx
@@ -0,0 +1,37 @@
+import { ImSearch } from 'react-icons/im';
+
+import styles from './SearchBox.module.css';
+import { selectNameFilter, changeFilter } from '../../redux/filtersSlice';
+import { useDispatch, useSelector } from 'react-redux';
+
+const SearchBox = () => {
+  const filter = useSelector(selectNameFilter);
+  const dispatch = useDispatch();
+  const handleChange = searchedValue => {
+    dispatch(changeFilter(searchedValue));
+  };
+
+  return (
+    <div>
+      <label className={styles.label}>
+        <span className={styles.label_title}>
+          Find contacts by name or number
+        </span>
+
+        <input
+          onChange={event => {
+            handleChange(event.target.value);
+          }}
+          className={styles.input}
+          type="text"
+          value={filter}
+          placeholder="Start typing..."
+        />
+
+        <ImSearch className={styles.ico} />
+      </label>
+    </div>
+  );
+};
+
+export default SearchBox;
---
+++ src/redux/store.js
@@ -0,0 +1,13 @@
+//? файл створення стору
+// src/redux/store.js
+
+import { configureStore } from '@reduxjs/toolkit';
+import { contactsReducer } from './contactsSlice';
+import { filtersReducer } from './filtersSlice';
+
+export const store = configureStore({
+  reducer: {
+    contacts: contactsReducer,
+    filters: filtersReducer,
+  },
+});
